36c9f301c46fd12e437b507977020f973a2cb0fd
andy <416548283@qq.com>
2018-07-09 23:47:23 +0800

    add redirect test

--- service-hi/src/main/java/com/forezp/ServiceHiApplication.java
+++ service-hi/src/main/java/com/forezp/ServiceHiApplication.java
@@ -5,13 +5,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
+//@RestController
+@Controller
 public class ServiceHiApplication {
 
 	public static void main(String[] args) {
@@ -23,6 +29,14 @@ public class ServiceHiApplication {
 	@RequestMapping("/hi")
 	public String home(@RequestParam String name) {
 		return "hi "+name+",i am from port:" +port;
+	}
+
+	@RequestMapping("/testRedirect")
+	public String testRedirect(HttpServletRequest req, HttpServletResponse rsp) {
+		Random random = new Random();
+		int randInt = random.nextInt(21_0000_0000);
+		String url = "redirect:" + "http://www.baidu.com"+ "/" + randInt;
+		return url;
 	}
 
 }
