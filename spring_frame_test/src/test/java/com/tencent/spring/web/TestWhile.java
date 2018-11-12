5649421af91f7c85d090e495c6d5e07e76a603fc
andy <416548283@qq.com>
2018-11-12 22:55:53 +0800

    add spring aop

---
+++ spring_frame_test/src/test/java/com/tencent/spring/web/TestWhile.java
@@ -0,0 +1,14 @@
+package com.tencent.spring.web;
+
+/**
+ * Created by andy on 2018/11/12.
+ */
+public class TestWhile {
+
+    public static void main(String[] args) {
+
+        while(true) {
+            System.out.println("hello");
+        }
+    }
+}
