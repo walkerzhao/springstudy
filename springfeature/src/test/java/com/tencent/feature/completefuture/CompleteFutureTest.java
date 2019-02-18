19ccd72cefcf23e8d9ec0510ad1c97cf6893e3fa
ewan <zhaoyu@huay.com>
2019-02-18 13:39:10 +0800

    add test

--- springfeature/src/test/java/com/tencent/feature/completefuture/CompleteFutureTest.java
+++ springfeature/src/test/java/com/tencent/feature/completefuture/CompleteFutureTest.java
@@ -2,10 +2,7 @@ package com.tencent.feature.completefuture;
 
 import org.junit.Test;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.*;
 
 /**
  * Created by andy on 2019/2/17.
@@ -96,8 +93,90 @@ public class CompleteFutureTest {
         System.out.println(cf.join());
     }
 
+    @Test
+    public void thenAcceptExample() {
+        StringBuilder result = new StringBuilder();
+        CompletableFuture.completedFuture("thenAccept message")
+                .thenAccept(s -> result.append(s));
+        System.out.println(result.length());
+    }
+
+    @Test
+    public void thenAcceptAsyncExample() {
+        StringBuilder result = new StringBuilder();
+        CompletableFuture cf = CompletableFuture.completedFuture("thenAcceptAsync message")
+                .thenAcceptAsync(s -> result.append(s));
+        cf.join();
+        System.out.println(result.length());
+    }
+
+    @Test
+    public void completeExceptionallyExample() {
+        CompletableFuture cf = CompletableFuture.completedFuture("message").thenApplyAsync(String::toUpperCase,
+                executor);
+        CompletableFuture exceptionHandler = cf.handle((s, th) -> {
+            return (th != null) ? "message upon cancel" : "";
+        });
+        cf.completeExceptionally(new RuntimeException("completed exceptionally"));
+        System.out.println(cf.isCompletedExceptionally());
+
+        try {
+            cf.join();
+            System.out.println("Should have thrown an exception");
+        } catch (CompletionException ex) { // just for testing
+            System.out.println(ex.getCause().getMessage());
+
+        }
+        System.out.println(exceptionHandler.join());
+
+    }
+
+
+    @Test
+    public void cancelExample() {
+        CompletableFuture cf = CompletableFuture.completedFuture("message").thenApplyAsync(String::toUpperCase,
+                executor);
+        CompletableFuture cf2 = cf.exceptionally(throwable -> "canceled message");
+        System.out.println(cf.cancel(true));
+        System.out.println(cf.isCompletedExceptionally());
+        System.out.println(cf2.join());
+    }
+
+
+    @Test
+    public void applyToEitherExample() {
+        String original = "Message";
+        CompletableFuture cf1 = CompletableFuture.completedFuture(original)
+                .thenApplyAsync(s -> {
+                    try {
+                        return delayedUpperCase(s);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return s;
+                    }
+                });
+        CompletableFuture cf2 = cf1.applyToEither(
+                CompletableFuture.completedFuture(original).thenApplyAsync(s -> delayedLowerCase(s)),
+                s -> s + " from applyToEither");
+        System.out.println(cf2.join());
+
+
+    }
+
+
     
 
+    private Object delayedLowerCase(String s) {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
 
-
+    private Object delayedUpperCase(String s) throws InterruptedException {
+        Thread.sleep(200);
+        return s;
+    }
 }
